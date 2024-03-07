57b0fbda6638516d7e38036a6f6efd38a1da78f4
jackson-hu1279 <68998854+jackson-hu1279@users.noreply.github.com>
2024-03-07 22:54:40 +1100

    Improve required cookies and tokens logic

--- scrape.py
+++ scrape.py
@@ -4,8 +4,26 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# URL of the web page you want to access
-url = 'https://apps.pcdirectory.gov.hk/Public/TC/AdvancedSearch?ProfID=RMP'
+
+# ==============
+# Search Name
+# ==============
+
+
+# Set name of doc to find
+doc_name = "CHAN KAI MING"
+
+# Import search parameters
+request_para = search_para.search_para
+request_para['Name'] = doc_name
+
+# Bypass token verification
+cookies = requests.cookies.RequestsCookieJar()
+cookies['__RequestVerificationToken_L1B1YmxpYw2'] = os.environ.get(
+    "__RequestVerificationToken_L1B1YmxpYw2")
+request_para['__RequestVerificationToken'] = os.environ.get(
+    "__RequestVerificationToken")
+
 headers = {
     'User-Agent': 'Mozilla/5.0',
     'Content-Type': 'application/x-www-form-urlencoded',
@@ -14,38 +32,11 @@ headers = {
     'sec-ch-ua-platform': '"macOS"',
     'Upgrade-Insecure-Requests': '1'
 }
-params = {'key': 'value'}
-
-
-# Set up cookies
-new_cookies = requests.cookies.RequestsCookieJar()
-new_cookies['NET_SessionId'] = os.environ.get("NET_SessionId")
-new_cookies['__RequestVerificationToken_L1B1YmxpYw2'] = os.environ.get(
-    "__RequestVerificationToken_L1B1YmxpYw2")
-
-# Print cookies
-for cookie in new_cookies:
-    cookie_name = cookie.name
-    cookie_value = cookie.value
-    print(f"Cookie Name: {cookie_name}")
-    print(f"Cookie Value: {cookie_value}\n")
-
-# ==============
-# Search Name
-# ==============
-
-doc_name = "CHAN KAI MING"
-
-# Import search parameters
-request_para = search_para.search_para
-request_para['__RequestVerificationToken'] = os.environ.get(
-    "__RequestVerificationToken")
-request_para['Name'] = doc_name
 
 # Sending POST request with the specified headers and data
 search_url = 'https://apps.pcdirectory.gov.hk/Public/TC/AdvancedSearch'
 response = requests.post(search_url, headers=headers,
-                         cookies=new_cookies, data=request_para)
+                         cookies=cookies, data=request_para)
 
 # Save if successfully get search reseults
 if response.status_code == 200:
