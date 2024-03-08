798530adbd544c65b9e8cde0001546f1a9f85f59
jackson-hu1279 <68998854+jackson-hu1279@users.noreply.github.com>
2024-03-08 12:14:53 +1100

    Find info in correct practice (current active)

--- doc_parser.py
+++ doc_parser.py
@@ -13,7 +13,7 @@ def extract_table_content(table_div):
 
         table_data += (row_data + '\n')
 
-    return table_data
+    return table_data.strip()
 
 # Extract care program info
 def extract_care_program(program_div):
@@ -24,6 +24,14 @@ def extract_care_program(program_div):
 
     return programs
 
+# Get current practice tab
+def get_current_practice_ref(page_content):
+    practice_ref = page_content.find('a', class_='active').get('href')
+    practice_ref = practice_ref.replace("#", "")
+
+    return practice_ref
+
+# Extract required doc info (main logic)
 def extract_doc_info(doc_counter):
     # Load HTML file
     with open(f'temp/doc_profile_{doc_counter}.html', 'r') as file:
@@ -33,6 +41,7 @@ def extract_doc_info(doc_counter):
     soup = BeautifulSoup(html_content, 'html.parser')
     doc_profile_div = soup.find('div', class_='Search-Box_container')
     doc_practice_div = doc_profile_div.find('div', class_='practice-detail-content')
+    practice_ref = get_current_practice_ref(soup)
 
     # Extract doc basic info
     profile_info = {}
@@ -46,7 +55,7 @@ def extract_doc_info(doc_counter):
 
     # Extract doc practice info
     required_fields = ["地址", "執業類別", "電話", "應診時間", "政府基層醫療促進計劃"]
-    current_pracice_div = doc_practice_div.find('div', id='lst001')
+    current_pracice_div = doc_practice_div.find('div', id=practice_ref)
     for legend_pair_div in current_pracice_div.findAll('div', class_='content-section'):
         legend = legend_pair_div.find('div', class_='legend').text.strip()
 
@@ -77,3 +86,4 @@ def extract_doc_info(doc_counter):
     return None
 
 extract_doc_info(1)
+
