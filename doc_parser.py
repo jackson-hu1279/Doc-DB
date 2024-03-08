2c06f668b2a5e3e6e58ea6b6c960924f7364ef54
jackson-hu1279 <68998854+jackson-hu1279@users.noreply.github.com>
2024-03-08 11:44:35 +1100

    Add function to process table content

--- doc_parser.py
+++ doc_parser.py
@@ -1,5 +1,19 @@
 from bs4 import BeautifulSoup
 
+def extract_table_content(table_div):
+    table_data = ""
+
+    # Iterate through each row in the table
+    for row in table_div.find_all('tr'):
+        row_data = ""
+        # Iterate through each cell in the row
+        for cell in row.find_all(['th', 'td']):
+            row_data += cell.text.strip()
+            
+        table_data += (row_data + '\n')
+
+    return table_data
+
 def extract_doc_info(doc_counter):
     # Load HTML file
     with open(f'temp/doc_profile_{doc_counter}.html', 'r') as file:
@@ -32,6 +46,9 @@ def extract_doc_info(doc_counter):
 
         info = legend_pair_div.find('div', class_='info').text.strip()
 
+        if legend == "應診時間":
+            table_data = extract_table_content(legend_pair_div.find('table'))
+            info = table_data
 
         if legend == "政府基層醫療促進計劃":
             plan = []
